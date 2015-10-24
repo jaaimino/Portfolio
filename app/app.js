0a7039a1efcc024ed2691dfa70664a5c291600e9
jaaimino <jacobaimino@gmail.com>
2015-10-24 16:47:30 -0400

    No rewrite support for github.

--- app/app.js
+++ app/app.js
@@ -11,5 +11,5 @@ angular.module('jaimino', [
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
       .otherwise('/');
-    $locationProvider.html5Mode(true);
+    //$locationProvider.html5Mode(true);
   });
