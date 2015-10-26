ceb7b2ec60dc1801c823795f9fec9d36652eec9f
jaaimino <jacobaimino@gmail.com>
2015-10-25 22:09:09 -0400

    Project pages set now

--- components/projects/projects.service.js
+++ components/projects/projects.service.js
@@ -6,16 +6,17 @@
 
 angular.module('jaimino')
   .factory('Projects', function () {
-    var project = function(name, link, img){
+    var project = function(name, link, img, target){
         this.name = name;
         this.img = img;
         this.link = link;
+        this.target = target;
     };
     var projects = [
-        new project("Nope", "/#/", "assets/img/project_thumbs/gemini_thumb.png"),
-        new project("Test", "/#/", "assets/img/project_thumbs/gemini_thumb.png"),
-        new project("Test", "/#/", "assets/img/project_thumbs/gemini_thumb.png"),
-        new project("Test", "/#/", "assets/img/project_thumbs/gemini_thumb.png")
+        new project("Financial Advisor App", "https://github.com/jaaimino/financial-advisor-app", "assets/img/project_thumbs/finadvise_thumb.png", "_blank"),
+        new project("Tastimals", "https://jaaimino.github.io/tastimals/", "assets/img/project_thumbs/tastimals_thumb.png", "_blank"),
+        new project("Dogebook", "https://github.com/jaaimino/dogebook", "assets/img/project_thumbs/dogebook_thumb.png", "_blank"),
+        new project("Gemini Simulator", "https://github.com/jaaimino/gemini-simulator", "assets/img/project_thumbs/gemini_thumb.png", "")
     ];
     var getProjects = function(){
         return projects;
